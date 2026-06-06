86ab34cfae6353cf1aaafd9d786957ceb7b5af0a
Aria Horak <147880431+aweeri@users.noreply.github.com>
2026-06-07 01:08:38 +0200

    Update core_apps.bat to include VCLibs installation

--- core_apps.bat
+++ core_apps.bat
@@ -17,7 +17,7 @@ winget settings --enable BypassCertificatePinningForMicrosoftStore
 echo.
 echo 3. Force-updating AppInstaller from GitHub and repairing sources...
 :: This bypasses the broken Store API and pulls the new winget engine directly.
-powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri 'https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -OutFile \"$env:TEMP\winget.msixbundle\"; Add-AppxPackage -Path \"$env:TEMP\winget.msixbundle\""
+powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx' -OutFile \"$env:TEMP\vclibs.appx\"; Add-AppxPackage -Path \"$env:TEMP\vclibs.appx\"; Invoke-WebRequest -Uri 'https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -OutFile \"$env:TEMP\winget.msixbundle\"; Add-AppxPackage -Path \"$env:TEMP\winget.msixbundle\""
 winget source reset --force
 winget source update
 
